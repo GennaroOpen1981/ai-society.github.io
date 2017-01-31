95adae3586d7a40ffe0adfd30131c97add64930c
scvalencia <sc.valencia606@uniandes.edu.co>
2017-01-31 17:13:09 -0500

    A simple recommender system

--- src/2016-12-19-Collaborative-Filtering/main.py
+++ src/2016-12-19-Collaborative-Filtering/main.py
@@ -1,3 +1,6 @@
+import math
+
+
 data = {
 	'Alan Perlis': { 
 		'Artificial intelligence': 1.46, 
@@ -118,3 +121,55 @@ def euclidean_similarity(person1, person2):
 	distance = [pow(rank[0] - rank[1], 2) for rank in rankings]
 
 	return 1 / (1 + sum(distance))
+
+def pearson_similarity(person1, person2):
+
+	common_ranked_items = [itm for itm in data[person1] if itm in data[person2]]
+
+	n = len(common_ranked_items)
+
+	s1 = sum([data[person1][item] for item in common_ranked_items])
+	s2 = sum([data[person2][item] for item in common_ranked_items])
+
+	ss1 = sum([pow(data[person1][item], 2) for item in common_ranked_items])
+	ss2 = sum([pow(data[person2][item], 2) for item in common_ranked_items])
+
+	ps = sum([data[person1][item] * data[person2][item] for item in common_ranked_items])
+
+	num = n * ps - (s1 * s2)
+
+	den = math.sqrt((n * ss1 - math.pow(s1, 2)) * (n * ss2 - math.pow(s2, 2)))
+
+	return (num / den) if den != 0 else 0
+
+
+
+def recommend(person, bound, similarity=pearson_similarity):
+	scores = [(similarity(person, other), other) for other in data if other != person]
+
+	scores.sort()
+	scores.reverse()
+	scores = scores[0:bound]
+
+	print scores
+
+	recomms = {}
+
+	for sim, other in scores:
+		ranked = data[other]
+
+		for itm in ranked:
+			if itm not in data[person]:
+				weight = sim * ranked[itm]
+
+				if itm in recomms:
+					s, weights = recomms[itm]
+					recomms[itm] = (s + sim, weights + [weight])
+				else:
+					recomms[itm] = (sim, [weight])
+
+	for r in recomms:
+		sim, item = recomms[r]
+		recomms[r] = sum(item) / sim
+
+	return recomms
